25ebc75ae28a926c4aea09dc678a055e4315a973
marcelodanieldm <danielmarcelo83@gmail.com>
2023-05-21 00:07:11 -0300

    exercise 1 day 2

---
+++ day2-1.py
@@ -0,0 +1,12 @@
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+type(two_digit_number) 
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+together = second_digit + first_digit
+print(together)
