80385d9e15e283fdaaa9a3a854f46aa4507ba939
marcelodanieldm <danielmarcelo83@gmail.com>
2024-11-07 16:31:29 -0300

    improved dictionary

--- coffee machine/main.py
+++ coffee machine/main.py
@@ -1,28 +1,32 @@
-MENU :{
+MENU = {
 
-resources = {
+"espresso": {
+    ingredients:{
+    "water": 50,
+    "coffee": 18,
+},
+    "cost":1.5,
+},
 
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+"latte": {
+    ingredients:{
+    "water": 200,
+    "milk": 150,
+    "coffee": 24,
+}, 
+"cost": 2.5 ,
+}, 
+
+"cappuccino": {
+    ingredients:{
+    "water": 250,
+    "milk": 100,
+    "coffee": 24,
+}, 
+"cost": 1.5 ,
+}
 }
 
-
-resources = {
-
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
-
-resources = {
-
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-}
 
 profit = 0
 resources = {
@@ -43,25 +47,26 @@ def is_resources_sufficient(order_ingredients):
 def process_coins():
     """Returns the total calculated from coins inserted"""
     print("Insert coin ")
-    total = int(input("How many quarters?: "))*0.25
-    total = int(input("How many dimes?: "))*0.1
-    total = int(input("How many nickles?: "))*0.05
-    total = int(input("How many pennies?"))*0.01
+    total += int(input("How many quarters?: "))*0.25
+    total += int(input("How many dimes?: "))*0.1
+    total += int(input("How many nickles?: "))*0.05
+    total += int(input("How many pennies?"))*0.01
 
     return total
 
 def is_transaction_successful(money_received, drink_cost):
     """Returns true when the payment is accepted, and false when there is no enough money """
     if money_received >= drink_cost:
+        change = round(money_received - drink_cost,2)
+        print(f"Here is the ${change} in change.")
+        global profit
+        profit += drink_cost
         return True
     else:
         print("Sorry there is no enough money. Money refunded.")
         return False
     
     
-    
-
-
 is_on = True
 
 
