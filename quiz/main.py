e49fdd9dbf3f69c41f2bb76d71f947f61f3350e2
marcelodanieldm <danielmarcelo83@gmail.com>
2024-11-08 15:49:58 -0300

    quiz brain

--- quiz/main.py
+++ quiz/main.py
@@ -1,6 +1,6 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 for question in question_data:
     question_text= question["text"]
@@ -8,5 +8,7 @@ for question in question_data:
     new_question= question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
-print(question_bank[0].answer)
+#print(question_bank[0].answer)
