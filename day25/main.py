c1cb1c04d532e7a46f4057b6e68726b2661e836c
marcelodanieldm <danielmarcelo83@gmail.com>
2025-06-13 16:55:01 -0300

    'working with columns and rows in Pandas'

--- day25/main.py
+++ day25/main.py
@@ -18,5 +18,21 @@ import pandas
 pandas.read_csv("wheater_data.csv")
 print(data)
 print(data["temp"])
-        
-     
+
+data_dict=data.to_dict()
+print(data_dict)
+
+temp_list=data["temp"].to_list()
+print(len(temp_list))
+
+print(data["temp"].mean())   
+print(data["temp"].max())
+
+# Get data in columns
+print(data["condition"])
+print(data.condition)
+      
+# Get data in rows
+print(data[data.day == "Monday"])
+data.temp==data.temp.max()
+    
